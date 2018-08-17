d790cfd58e7c87e001db9644305f80aa2a849277
Timothy Liu <tliu12@berkeley.edu>
2018-08-17 12:26:07 -0700

    Fixed styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Timothy Liu</h1>
+      <div className="index">
+        <header className="body">
+          <h1 className="title">Timothy Liu</h1>
+            <br />
+            <p className="sub-header">Education</p>
+            <p>University of California, Berkeley</p>
+            <p>Computer Science</p>
+            <p>Class of 2020</p>
+            <br />
+            <p className="sub-header">Experience</p>
+            <p><a href="https://www.shell.com/energy-and-innovation/innovating-together/shell-techworks.html" target="_blank">Shell TechWorks</a></p>
+            <p><a href="https://swarmlab.berkeley.edu/home" target="_blank">The Ubiquitous SWARM Lab</a></p>
+            <br />
+            <p className="sub-header">Organizations</p>
+            <p><a href="https://uav.berkeley.edu/" target="_blank">UAVs@Berkeley</a></p>
+            <p><a href="http://callaunchpad.org/#/" target="_blank">Launchpad</a></p>
+            <p><a href="https://csmberkeley.github.io/#/" target="_blank">Computer Science Mentors</a></p>
+            <br />
+            <p className="sub-header">Miscellaneous</p>
+            <p><a href="../public/timothy_liu_resume.pdf" target="_blank">Resume</a></p>
+            <p><a href="https://github.com/tbliu" target="_blank">GitHub</a></p>
+            <p>Thoughts</p>
         </header>
-        <p className="App-intro">
-            This site is a work in progress
-        </p>
       </div>
     );
   }
