1dd3beb9c535ddfb445749dc8f438e5649a9fadf
Timothy Liu <timothy.b.liu@gmail.com>
2026-02-19 18:52:35 -0800

    Fixing some margin and padding mismatches

--- components/sidebar.js
+++ components/sidebar.js
@@ -9,7 +9,7 @@
         <span style="font-family:'Noe Standard','Times New Roman',serif;font-weight:400;font-size:32px;">Liu</span>
       </a>
 
-      <ul style="padding-top:1rem;list-style:none;padding-left:0;font-size:12px;text-align:left;">
+      <ul style="padding-top:1rem;list-style:none;padding-left:0;margin:0;font-size:12px;text-align:left;">
         <li style="margin-top:4px;"><a href="https://github.com/tbliu" target="_blank">GitHub</a></li>
         <li style="margin-top:4px;"><a href="https://www.linkedin.com/in/tbliu/" target="_blank">LinkedIn</a></li>
         <li style="margin-top:4px;"><a href="https://twitter.com/tbliu" target="_blank">Twitter</a></li>
